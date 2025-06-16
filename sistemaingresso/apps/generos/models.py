019d398556647efa36235d05585c7914b8268813
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-16 17:36:38 -0300

    Initial Commit

--- sistemaingresso/apps/generos/models.py
+++ sistemaingresso/apps/generos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Genero(models.Model):
+    nome = models.CharField('nome', max_length=100, unique=True)
+    descricao = models.TextField('descricao', blank=True, null=True)
+
+class Meta:
+        verbose_name = 'Generos'
+        verbose_name_plural = 'Generos'
+        ordering =['id']
+
+def __str__(self):
+        return self.nome
