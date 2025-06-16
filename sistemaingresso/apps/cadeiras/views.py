9d153fe0c43b10861b00191ccedddc6e93051a0e
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-16 18:43:00 -0300

    FinalCommit

--- sistemaingresso/apps/cadeiras/views.py
+++ sistemaingresso/apps/cadeiras/views.py
@@ -1,3 +1,10 @@
+from .models import Cadeira
+from rest_framework import viewsets
+from .serializer import CadeiraSerializer
 from django.shortcuts import render
 
 # Create your views here.
+
+class CadeiraViewSet(viewsets.ModelViewSet):
+    queryset = Cadeira.objects.all()
+    serializer_class = CadeiraSerializer  
--- sistemaingresso/apps/filmes/apps.py
+++ sistemaingresso/apps/filmes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FilmesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filmes'
+    verbose_name = "Filme"
