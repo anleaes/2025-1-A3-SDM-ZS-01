41c892a140ccac99cdf17b1a8a0a23ac48ea4d4c
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-17 15:33:49 -0300

    itemcompras criada

---
+++ sistemaingresso/apps/itemcompras/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ItemcomprasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'itemcompras'
+    verbose_name = 'Itens de Compras'
