786cdbb982cdd33463b71547063346ba8e6eaf25
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-17 14:56:35 -0300

    Imports da views

--- sistemaingresso/apps/sessoes/views.py
+++ sistemaingresso/apps/sessoes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Sessao
+from rest_framework import viewsets
+from .serializer import SessaoSerializer
+from django.shortcuts import render
 
 # Create your views here.
