5899bba22e7c6457b7180a34ebb0640f8120d108
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-17 14:47:46 -0300

    Classe Sessao Criada

--- sistemaingresso/apps/sessoes/apps.py
+++ sistemaingresso/apps/sessoes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SessoesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sessoes'
+    verbose_name = "Sessao"
--- sistemaingresso/apps/sessoes/models.py
+++ sistemaingresso/apps/sessoes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from .models import Filme
 # Create your models here.
+
+class Sessao(models.Model):
+    """
+    Representa uma exibição de um filme em um horário e sala específicos.
+    """
+    filme = models.ForeignKey(Filme, on_delete=models.CASCADE, related_name="sessoes") 
+    sala = models.PositiveIntegerField('sala')
+    horario = models.DateTimeField('horario', help_text="Data e hora de início da sessão.") 
+    is_active = models.BooleanField('ativo',default=True)  
+
+    def __str__(self):
+        return f"{self.filme.titulo} - Sala {self.sala} ({self.horario.strftime('%d/%m/%Y %H:%M')})"
+
+    class Meta:
+        verbose_name = "Sessao"
+        verbose_name_plural = "Sessoes"
