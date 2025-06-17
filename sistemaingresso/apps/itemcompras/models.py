18f7962b2d9910deb408a9a44ba735a8322429e3
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-17 16:06:00 -0300

    Models ItemCompra Atualizado

--- sistemaingresso/apps/itemcompras/models.py
+++ sistemaingresso/apps/itemcompras/models.py
@@ -8,8 +8,8 @@ from ingressos.models import Ingresso
 class Itemcompra(models.Model):
     quantitade = models.PositiveIntegerField('Quantidade',null=True, blank=True,default=0)
     preco_unitario = models.DecimalField('Preco unitario', max_digits=10, decimal_places=2)
-    ingresso = models.ForeignKey(Ingresso, on_delete=models.CASCADE)
-    compra = models.ForeignKey(Compra, on_delete=models.CASCADE)
+    ingresso = models.ForeignKey(Ingresso, on_delete=models.CASCADE, null=True, blank=True)
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         unique_together = ('compra', 'ingresso')
