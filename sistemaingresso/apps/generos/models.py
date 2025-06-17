e0c40e4f12438c2f028f3f841334f027936e3a97
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-17 15:39:08 -0300

    models Atualizado

--- sistemaingresso/apps/generos/models.py
+++ sistemaingresso/apps/generos/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class Genero(models.Model):
     nome = models.CharField('nome', max_length=100, unique=True)
     descricao = models.TextField('descricao', blank=True, null=True)
-
-class Meta:
+    class Meta:
         verbose_name = 'Generos'
         verbose_name_plural = 'Generos'
         ordering =['id']
 
-def __str__(self):
+    def __str__(self):
         return self.nome
+
--- sistemaingresso/apps/itemcompras/models.py
+++ sistemaingresso/apps/itemcompras/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from compras.models import Compra
+from ingressos.models import ingresso
+
 
 # Create your models here.
+
+class Orderitem(models.Model):
+    quantitade = models.PositiveIntegerField('Quantidade',null=True, blank=True,default=0)
+    preco_unitario = models.DecimalField('Preco unitario', max_digits=10, decimal_places=2)
+    ingresso = models.ForeignKey(Ingresso, on_delete=models.CASCADE)
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('Compra', 'ingresso')
+        verbose_name = 'Item de pedido'
+        verbose_name_plural = 'Itens de pedido'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.quantity} - {self.ingresso.name}"
