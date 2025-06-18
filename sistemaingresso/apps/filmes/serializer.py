9f19948491b20cd7ba455badb961457d912fb53b
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-18 20:13:11 -0300

    Serialzer changes

--- sistemaingresso/apps/filmes/serializer.py
+++ sistemaingresso/apps/filmes/serializer.py
@@ -2,6 +2,8 @@ from .models import Filme
 from rest_framework import serializers
 
 class FilmeSerializer(serializers.ModelSerializer):
+    generos = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Filme
         fields = '__all__'
