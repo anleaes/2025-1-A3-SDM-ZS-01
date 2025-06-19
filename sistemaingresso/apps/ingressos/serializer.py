14f00a6f5401f4a66aba4d0c2502ce076982e3d0
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-18 22:16:45 -0300

    feat: StringRelatedField para alguns objetos

--- sistemaingresso/apps/ingressos/serializer.py
+++ sistemaingresso/apps/ingressos/serializer.py
@@ -2,7 +2,14 @@ from .models import Ingresso
 from rest_framework import serializers
     
 class IngressoSerializer(serializers.ModelSerializer):
+    sessao_details = serializers.StringRelatedField(source='sessao', read_only=True)
+    cadeira_details = serializers.StringRelatedField(source='cadeira', read_only=True)
 
     class Meta:
         model = Ingresso
         fields = '__all__'
+
+        extra_kwargs = {
+            'sessao': {'write_only': True},
+            'cadeira': {'write_only': True}
+        }
--- sistemaingresso/apps/itemcompras/serializer.py
+++ sistemaingresso/apps/itemcompras/serializer.py
@@ -2,6 +2,9 @@ from .models import Itemcompra
 from rest_framework import serializers
     
 class ItemcompraSerializer(serializers.ModelSerializer):
+    compra_details = serializers.StringRelatedField(source='compra', read_only=True)
+    ingresso_details = serializers.StringRelatedField(source='ingresso', read_only=True)
+    
     class Meta:
         model = Itemcompra
         fields = '__all__'
